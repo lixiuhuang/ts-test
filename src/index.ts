a8e93064ad6104a954497ad1880216cbd3accfad
lixiuhuang <18910370212@163.com>
2020-11-05 10:17:23 +0800

    练习

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 console.log('hello typescript')
+let a = 1 + 2
+let b = a + 3
+let c = {
+  apple: a,
+  banana: b
+}
+let d = c.apple * 4
+
+function add(n: number) {
+  return n * n
+}
+add(2)
+
+let a: {
+  b: number
+  c?: string
+  [key: number]: boolean
+}
